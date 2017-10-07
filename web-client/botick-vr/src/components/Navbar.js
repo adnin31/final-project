55c4ed1bc5de892d9fee85b6851c9b49fc840895
rama841 <kickyou44@gmail.com>
2017-10-07 17:21:37 +0700

    register with modal

--- web-client/botick-vr/src/components/Navbar.js
+++ web-client/botick-vr/src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import { BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import { connect } from 'react-redux'
-import { postLogin, logOut } from '../actions/index.js'
+import { postLogin, logOut, getToken } from '../actions/index.js'
 
 
 class Navbar extends Component {
@@ -12,6 +12,9 @@ class Navbar extends Component {
       password: '',
       email: ''
     }
+  }
+  componentWillMount () {
+    this.props.willToken(localStorage.getItem('token'))
   }
   handleChange(event) {
     this.setState({
@@ -40,7 +43,11 @@ class Navbar extends Component {
             <div className = "navbar-collapse collapse" id = "bs-example-navbar-collapse-1" aria-expanded="false" style={{height: 1}}>
               <ul className="nav navbar-nav navbar-right">
                 { this.props.token ? <li><a className= 'btn' onClick= {() => this.props.logout('')}>Logout</a></li>  :
-                <li><a className= 'btn' data-toggle="modal" data-target="#myModal" >Login</a></li> }
+                  <li style={{display: 'flex', alignItems: 'baseline'}}>
+                    <a className= 'btn' data-toggle="modal" data-target="#myModal" >Login</a> or <a className= 'btn' data-toggle="modal" data-target="#myRegister" >Register</a>
+                  </li>
+
+               }
 
               </ul>
             </div>
@@ -58,7 +65,7 @@ class Navbar extends Component {
                 <div className="modal-body" style={{'padding':'40px 50px'}}>
                   <form role="form">
                     <div className="form-group">
-                      <label for="usrname"><span className="glyphicon glyphicon-user"></span> Username</label>
+                      <label><span className="glyphicon glyphicon-user"></span> Username</label>
                     <input name= 'username' type="text" className= "form-control"  placeholder= "Enter user"  value= {this.state.username} onChange= {(e) => this.handleChange(e)}/>
                     </div>
                     <div className="form-group">
@@ -66,6 +73,43 @@ class Navbar extends Component {
                     <input name= 'password' type="password" className="form-control" placeholder="Enter password" value= {this.state.password} onChange= {(event) => this.handleChange(event)}/>
                     </div>
                     <button  type="button" className="btn btn-success btn-block" data-dismiss="modal" onClick={() => this.props.postLogin(this.state)}><span className="glyphicon glyphicon-off"></span> Login</button>
+                  </form>
+                </div>
+                <div className="modal-footer">
+                  <button type="submit" className="btn btn-danger btn-default pull-left" data-dismiss="modal"><span className="glyphicon glyphicon-remove"></span> Cancel</button>
+                  <p>Not a member? <a href="#">Sign Up</a></p>
+                </div>
+              </div>
+            </div>
+        </div>
+
+        <div className="modal fade" id="myRegister" role="dialog">
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header" style={{'padding':'35px 50px'}} >
+                  <button type="button" className="close" data-dismiss="modal">&times;</button>
+                <h4><span className="glyphicon glyphicon-pencil"></span> Register</h4>
+                </div>
+                <div className="modal-body" style={{'padding':'40px 50px'}}>
+                  <form role="form">
+
+                    <div className="form-group">
+                      <label><span className="glyphicon glyphicon-user"></span> Username</label>
+                    <input name= 'username' type="text" className= "form-control"  placeholder= "Enter user"  value= {this.state.username} onChange= {(e) => this.handleChange(e)}/>
+                    </div>
+
+                    <div className="form-group">
+                      <label for="psw"><span className="glyphicon glyphicon-eye-open"></span> Password</label>
+                    <input name= 'password' type="password" className="form-control" placeholder="Enter password" value= {this.state.password} onChange= {(event) => this.handleChange(event)}/>
+                    </div>
+
+                    <div className="form-group">
+                      <label for="psw"><span className="glyphicon glyphicon glyphicon-envelope"></span> Email </label>
+                    <input name= 'email' type="password" className="form-control" placeholder="Enter password" value= {this.state.email} onChange= {(event) => this.handleChange(event)}/>
+                    </div>
+
+                    <button  type="button" className="btn btn-success btn-block" data-dismiss="modal" onClick={() => this.props.postLogin(this.state)}><span className="glyphicon glyphicon-off"></span> Login</button>
+
                   </form>
                 </div>
                 <div className="modal-footer">
@@ -94,6 +138,9 @@ class Navbar extends Component {
       },
       logout: (data) => {
         dispatch(logOut(data))
+      },
+      willToken: (data) => {
+        dispatch(getToken(data))
       }
     }
 }
